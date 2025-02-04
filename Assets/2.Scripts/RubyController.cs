d13af3049ca27261164a7af03b4b8aa7f03f2834
SJ9211 <jsj921127@naver.com>
2025-02-04 15:48:54 +0900

    X.Y축 이동

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
     
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;      
+        //Application.targetFrameRate = 10;    0.1*10 으로 이동하여 초당 1유닛만큼 이동        
     }
 
     
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // GetAxisLaw를 사용하면 -1,1값이 넘어온다
+        //float vertical = Input.GetAxisRaw("Vertical");
+        // Debug.Log($"H:{horizontal}");
+        // Debug.Log($"V:{vertical}");
+        //Debug.Log(horizontal);
+        //Debug.LogWarning("Warnig");
+        //Debug.LogError("Error!!!");
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += moveSpeed * horizontal * Time.deltaTime;
+        position.y += moveSpeed * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
