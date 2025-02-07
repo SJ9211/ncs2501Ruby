44726ab95772de052d5cc3edeeab662b5abf314e
SJ9211 <jsj921127@naver.com>
2025-02-07 15:45:23 +0900

    item먹기

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    public float moveSpeed = 5.0f;
+    public int maxHealth = 5;
 
+    public float moveSpeed = 5.0f;
+    public int health { get { return currentHealth; } }
+    private int currentHealth;
+    private Rigidbody2D rb2d;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         //QualitySettings.vSyncCount = 0;      
         //Application.targetFrameRate = 10;    0.1*10 으로 이동하여 초당 1유닛만큼 이동        
-    }
 
+        currentHealth = maxHealth;
+    }
 
     void Update()
     {
@@ -31,4 +35,14 @@ public class RubyController : MonoBehaviour
         position.y += moveSpeed * vertical * Time.deltaTime;
         rb2d.MovePosition(position);
     }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        //Debug.Log($"{currentHealth / maxHealth}");  // string 보관용
+       Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
+/*
+충돌 ~ collider  {OnCollisionEnter2D, OnCollisionStay2D, OnCollisionExit2D}
+                  OnTriggerEnter2D, OnTriggerStay2D , OnTriggerExit2D
+*/
